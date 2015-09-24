3ab0db480fc294d9d5d86626089cd16e881e3d7d
Cecilia Nilsson <c.e.l.nilsson@gmail.com>
2015-09-24 17:04:14 +0200

    Grundprogram

---
+++ GirlMobContactList/GirlMobContactList/WebUserControl1.ascx
@@ -0,0 +1,24 @@
+﻿<%@ Control Language="C#" AutoEventWireup="true" CodeBehind="WebUserControl1.ascx.cs" Inherits="GirlMobContactList.WebUserControl1" %>
+    <div>
+       <asp:Label ID="LabelFirstName" runat="server" Text="First name"></asp:Label>
+    </div>
+    <div>
+        <asp:TextBox ID="TextBoxFirstName" runat="server"></asp:TextBox>
+    </div>
+    <div>
+        <asp:Label ID="LabelLastname" runat="server" Text="Last name"></asp:Label>
+    </div>
+    <div>
+        <asp:TextBox ID="TextBoxLastName" runat="server"></asp:TextBox>
+    </div>
+    <div>
+        <asp:Label ID="LabelSSN" runat="server" Text="SSN"></asp:Label>
+    </div>
+    <div>
+        <asp:TextBox ID="TextBoxSSN" runat="server"></asp:TextBox>
+    </div>
+    <div>
+        <asp:Button ID="ButtonChangeable" runat="server" Text="Button" />
+    </div>
+<asp:Label ID="LabelError" Text="" ForeColor="Red" runat="server" Font-Bold="False" />
+<asp:Label ID="LabelInfo" Text="" ForeColor="Green" runat="server" Font-Bold="False" />
---
+++ GirlMobContactList/GirlMobContactList/WebUserControl1.ascx.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace GirlMobContactList
+{
+    public partial class WebUserControl1 : System.Web.UI.UserControl
+    {
+        private const string myConnectionString = (@"Data Source=ACADEMY27-VM\SQLEXPRESS; Initial Catalog=Contacts;Integrated Security=SSPI");
+        SqlConnection myConnection = new SqlConnection();
+        SqlCommand command = new SqlCommand();
+        private string action;
+        private int id;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+        }
+
+        public void InitContactForm(string action, int id = 0)
+        {
+            this.action = action;
+            this.id = id;
+
+            switch (action)
+            {
+                case "Add":
+                    SetAddMode();
+                    break;
+                case "Update":
+                    if (id != 0)
+                    {
+                        SetUpdateMode();
+                    }
+                    else
+                    {
+                        SetLockedMode();
+                        LabelError.Text = "No valid ID was given.";
+                    }
+                    break;
+                case "Delete":
+                    if (id != 0)
+                    {
+                        SetDeleteMode();
+                    }
+                    else
+                    {
+                        SetLockedMode();
+                        LabelError.Text = "No valid ID was given.";
+                    }
+                    break;
+                default:
+                    break;
+            }
+
+        }
+
+        private void SetAddMode()
+        {
+            ButtonChangeable.Text = "Add";
+
+            //LabelTitle.Text = "Add contact";
+            //LabelID.Visible = false;
+            //LabelIDValue.Visible = false;
+        }
+
+        private void SetUpdateMode()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void SetDeleteMode()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void SetLockedMode(bool setLock = false)
+        {
+            foreach (var control in this.Controls)
+                if (control is TextBox)
+                    ((TextBox)control).Enabled = setLock;
+                else if (control is Button)
+                    ((Button)control).Enabled = setLock;
+
+        }
+
+    }
+}
